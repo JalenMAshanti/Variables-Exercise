d4202336bec3594e178a67f3f0c77f6b9971589b
Jalen Ashanti <jalenmshaw@gmail.com>
2024-05-31 12:19:40 -0400

    Dog Story with Readline Bonus

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a dog name:");
+
+            string? dogName = Console.ReadLine();
+
+            if (dogName == "") {
+                dogName = "Duke";           
+            }
+
+            int dogAge = 4;
+            char dogMiddleInitial = 'A';
+            bool isDogHappy = true;
+            double poundsOfFood = 22.7;
+            decimal dogFoodCost = 1543.98m;
+
+            Console.WriteLine($"My dogs name is {dogName} and his middle name start with {dogMiddleInitial}. he is {dogAge} years old today.\nFor his birthday we got him {poundsOfFood} pounds of food" +
+                $" and it costed ${dogFoodCost}. It is {isDogHappy} that {dogName} was happy.");
+            Console.ReadLine();
+                          
         }
     }
 }
